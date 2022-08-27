d2c14a448bdd244bb2d06cd96793c3fce613dd20
Taehee Kim <rabolution@gmail.com>
2022-08-27 12:31:20 +0900

    refactor: 에러 테스트, 하드코딩

--- back/src/comments/repository.test.ts
+++ back/src/comments/repository.test.ts
@@ -1,27 +1,31 @@
-import { FakeRepo, CommentRepository } from './repository';
+import { FakeRepo } from './repository';
+
+const POST_ID_1 = 1;
 
 // 포스트를 작성하면, 추가된다!
 describe('comment', () => {
   it('scenario', async () => {
-    const repo: CommentRepository = FakeRepo();
-    const NEW_COMMENT = {
-      message: '안녕',
-    };
+    const repo = FakeRepo();
     // 처음에는 id가 1인 post의 댓글 목록은 비어있다.
-    expect(await repo.getCommentsByPostId(1)).toStrictEqual([]);
+    expect(await repo.getCommentsByPostId(POST_ID_1)).toStrictEqual([]);
     // post가 없으면 댓글목록을 가져올 수 없다.
     // toThrowError가 작동하지 않아서 주석처리
-    // expect(await repo.getCommentsByPostId(2)).toThrowError('404')
+    expect(() => repo.getCommentsByPostId(2)).rejects.toThrowError(
+      '404 not found'
+    );
 
     // id가 1인 post의 댓글을 추가한다.
-    await repo.addCommentToPost(1, NEW_COMMENT);
-    // id가 1인 post의 댓글이 추가되었다.
-    expect(await repo.getCommentsByPostId(1)).toStrictEqual([
-      { id: 1, ...NEW_COMMENT },
+    await repo.addCommentToPost(POST_ID_1, {
+      message: '안녕',
+    });
+    // id가 1인 post에 댓글이 추가되었다.
+    expect(await repo.getCommentsByPostId(POST_ID_1)).toStrictEqual([
+      { id: 1, message: '안녕' },
     ]);
   });
+  
   it('autoincrement', async () => {
-    const repo: CommentRepository = FakeRepo({
+    const repo = FakeRepo({
       1: [
         { id: 2, message: '안녕' },
         { id: 4, message: '안녕2' },
--- back/src/comments/router.spec.ts
+++ back/src/comments/router.spec.ts
@@ -1,27 +1,31 @@
 import { router } from '@trpc/server';
-import { commentRouter } from "./router";
+import { commentRouter } from './router';
 
-describe("comments", () => {
+const POST_ID_1 = 1;
+
+describe('comments', () => {
   it('scenario', async () => {
     const ctx = {};
     // caller를 만들고
-    const caller = router()
-      .merge('comment.', commentRouter)
-      .createCaller(ctx);
-
-      const NEW_COMMENT = {
-        message: "안녕"
-      }
+    const caller = router().merge('comment.', commentRouter).createCaller(ctx);
 
     // 처음에는 id가 1인 post의 댓글 목록은 비어있다.
-    expect(await caller.query('comment.read', 1)).toStrictEqual([])
+    expect(await caller.query('comment.read', POST_ID_1)).toStrictEqual([]);
     // id가 1인 post의 댓글을 추가한다.
-    await caller.mutation('comment.create', {id: 1, comment: NEW_COMMENT})
+    await caller.mutation('comment.create', {
+      id: POST_ID_1,
+      comment: {
+        message: '안녕',
+      },
+    });
     // id가 1인 post의 댓글이 추가되었다.
-    expect(await caller.query('comment.read', 1)).toStrictEqual([{
-      id: 1,
-      ...NEW_COMMENT
-    }])
+    expect(await caller.query('comment.read', POST_ID_1)).toStrictEqual([
+      {
+        id: 1,
+        message: '안녕',
+      },
+    ]);
 
+    expect(() => caller.query('comment.read', 2)).rejects.toThrowError('404');
   });
 });
