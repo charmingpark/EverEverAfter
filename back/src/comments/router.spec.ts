b99b543d50a41bb9a43b5da07084983e8c9b685c
chloeseo0210 <chloeseo0210@gmail.com>
2022-09-01 17:22:21 +0900

    feat : update, delete router 구현

--- back/src/comments/router.spec.ts
+++ back/src/comments/router.spec.ts
@@ -10,6 +10,8 @@ describe('comments', () => {
       commentRepo: FakeCommentRepo({ [POST_ID_1]: [] })
     });
 
+    expect(() => caller.query('read', 2)).rejects.toThrowError('404');
+
     // 처음에는 id가 1인 post의 댓글 목록은 비어있다.
     expect(await caller.query('read', POST_ID_1)).toStrictEqual([]);
     // id가 1인 post의 댓글을 추가한다.
@@ -27,6 +29,26 @@ describe('comments', () => {
       },
     ]);
 
-    expect(() => caller.query('read', 2)).rejects.toThrowError('404');
+    await caller.mutation('update', {
+      postId: POST_ID_1,
+      commentId: 1, 
+      message:'바이'
+    })
+
+    expect(await caller.query('read', POST_ID_1)).toStrictEqual([
+      {
+        id: 1,
+        message: '바이',
+      },
+    ]);
+
+    // mutation으로 데이터를 변경한다
+    await caller.mutation('delete', {
+      postId: POST_ID_1,
+      commentId: 1, 
+    })
+
+    // 기대하는대로 데이터가 변경되었는지, query해서 확인한다
+    expect(await caller.query('read', POST_ID_1)).toStrictEqual([]);
   });
 });
