c0d7c0931005c2f6b8986445f080dd85e7381012
ohbin-kwon <kobbb2100@gmail.com>
2022-10-19 13:47:57 +0900

    chore: serverless offline set up

--- back/src/createContext.ts
+++ back/src/createContext.ts
@@ -8,6 +8,9 @@ export type ContextT = PostRepoContext & CommentRepoContext;
 const postRepo = FakePostRepo([]);
 const commentRepo = FakeCommentRepo([]);
 
+// lambda event를 받을 상황이 있으면 context에 event를 주입받아야 할 것이다.
+// https://trpc.io/docs/v9/aws-lambda#3-use-the-amazon-api-gateway-adapter
+
 export async function createContext(): Promise<ContextT> {
   return { postRepo, commentRepo };
 }
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,31 +1,8 @@
-import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
-import fastify from 'fastify';
+import { awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda';
 import { createContext } from './createContext';
 import { appRouter } from '.';
-import cors from '@fastify/cors';
 
-const server = fastify({
-  maxParamLength: 100,
-  logger: true
+export const handler = awsLambdaRequestHandler({
+  router: appRouter,
+  createContext,
 });
-
-void server.register(cors, {
-  origin: ['http://localhost:3000']
-});
-
-void server.register(fastifyTRPCPlugin, {
-  prefix: '/trpc',
-  trpcOptions: { router: appRouter, createContext },
-});
-
-void (async () => {
-  try {
-    await server.listen({
-      host: '0.0.0.0',
-      port: 8000
-    });
-  } catch (err) {
-    server.log.error(err);
-    process.exit(1);
-  }
-})();
