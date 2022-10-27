baa92e0c89a0ca3edc136d72a0510c1cdf58b773
Taehee Kim <rabolution@gmail.com>
2022-10-27 14:21:07 +0900

    feat: add cors header

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,4 +5,28 @@ import { appRouter } from '.';
 export const handler = awsLambdaRequestHandler({
   router: appRouter,
   createContext,
+  responseMeta() {
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token",
+      },
+    };
+  },
 });
+
+
+// function getAccessControlAllowOrigin(stage: string) {
+//   if (stage === "dev") {
+//     return "*";
+//   } else if (stage === "test") {
+//     return "https://test.example.com";
+//   } else if (stage === "prod") {
+//     return "https://example.com";
+//   } else {
+//     throw new Error(
+//       "Could not determine Access-Control-Allow-Origin from stage env var"
+//     );
+//   }
+// }
--- front/src/post/PostPage.tsx
+++ front/src/post/PostPage.tsx
@@ -6,7 +6,8 @@ import PostList from './PostList';
 
 export const queryClient = new QueryClient();
 const trpcClient = trpc.createClient({
-  url: 'https://61pif65yuf.execute-api.ap-northeast-2.amazonaws.com',
+  url: 'https://61pif65yuf.execute-api.ap-northeast-2.amazonaws.com'
+  // url: 'http://localhost:8000',
 });
 
 export default function PostPage() {
